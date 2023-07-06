9c6e801c257532ae792f098764623d18713add58
jigar <jigsoni75@gmail.com>
2023-07-06 22:22:25 +0100

    firstcommit

---
+++ src/lerning.java
@@ -0,0 +1,15 @@
+public class lerning {
+
+    static String Name="jigar";
+    static String surname="soni";
+    static int age =35;
+    static String profession= "SoftwareTester";
+
+
+    public static void main(String[] args) {
+        System.out.println("My name is " + Name + ".");
+        System.out.println("My surname  is " + surname+".");
+        System.out.println("My age is " + age+".");
+        System.out.println("My profession is " +profession+".");
+    }
+}
